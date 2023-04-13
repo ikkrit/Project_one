0110133a59998988aae7e1130e6142d5b7bdca73
ikkrit <johan.car@protonmail.com>
2023-04-13 19:07:53 +0200

    second

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [compteur, setCompteur] = useState(1);
+
+  const handleClick = () => {
+    
+  }
+
   return (
-    <div className="App">
-      
+    <div>
+      <h1>{ compteur }</h1>
+      <button onClick={ handleClick }>Incrémente</button>
     </div>
-  );
+  )
 }
+
 
 export default App;
